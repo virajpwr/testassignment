161569559a4e6a43b7efea81e35a8dfad36ab6f1
Viraj.Pawar <Viraj.Pawar@contractor.rhimagnesita.com>
2024-04-01 15:13:48 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st 
+import pandas as pd 
+# Set the app title 
+st.title('Assignment 2') 
+# Add a welcome message 
+st.write('Welcome to my Streamlit app!') 
+# Create a text input 
+widgetuser_input = st.text_input('Enter a search query', 'Hello, Streamlit!') 
+
+st.write('Customized Message:', widgetuser_input)
+
+df = pd.read_csv(r'./data/data.csv')
+
+st.write(df)
